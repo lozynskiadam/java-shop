89b68c134dfcc5ecc0bfb5c7be651a78f687cfdc
Adam Łożyński <lozynskiadam@gmail.com>
2022-12-18 17:58:34 +0100

    products view

--- src/main/java/lozynskiadam/shop/controllers/ProductController.java
+++ src/main/java/lozynskiadam/shop/controllers/ProductController.java
@@ -3,13 +3,15 @@ package lozynskiadam.shop.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MainController {
+@RequestMapping(path = "/products")
+public class ProductController {
 
-    @GetMapping("/")
+    @GetMapping(path = {"", "/"})
     public String index(Model model) {
-        return "pages/dashboard/index";
+        return "pages/products/index";
     }
 
 }
