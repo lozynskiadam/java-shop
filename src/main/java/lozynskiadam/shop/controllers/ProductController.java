e693d5c2db9ea9f7a325d13d14aa2ce6e8cebeaa
Adam Łożyński <lozynskiadam@gmail.com>
2023-01-05 21:13:33 +0100

    mysql

--- src/main/java/lozynskiadam/shop/controllers/ProductController.java
+++ src/main/java/lozynskiadam/shop/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package lozynskiadam.shop.controllers;
 
+import lozynskiadam.shop.repositories.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(path = "/products")
 public class ProductController {
 
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @GetMapping(path = {"", "/"})
     public String index(Model model) {
+        model.addAttribute("products", this.productRepository.findAll());
+
         return "pages/products/index";
     }
 
---
+++ src/main/java/lozynskiadam/shop/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package lozynskiadam.shop.repositories;
+
+import lozynskiadam.shop.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    List<Product> findAll();
+
+    Product findById(int id);
+
+}
